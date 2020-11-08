33f23a02097b9c1645d09b6613d3df303aa71cb3
zishkaz <loktevsergay.2003@gmail.com>
2020-11-08 21:01:40 +0300

    Solution 3
    Minor bug fixes

--- src/comparer.c
+++ src/comparer.c
@@ -6,44 +6,44 @@
 
 struct metadata_t {
 
-    unsigned int file_size;
-    unsigned int pixelar_offset;
-    unsigned int bitmap_width;
-    int bitmap_height;
+    unsigned long file_size;
+    unsigned long pixelar_offset;
+    unsigned long bitmap_width;
+    long bitmap_height;
     unsigned char bits_per_pixel;
-    unsigned int image_size;
+    unsigned long image_size;
     unsigned char bitmap_header_remains[16];
 };
 
 struct BMP {
 
-    unsigned int palette[256];
-    unsigned int *pixel_array;
+    unsigned long palette[256];
+    unsigned long *pixel_array;
 };
 
-unsigned int hash(unsigned char r, unsigned char g, unsigned char b) {
+unsigned long hash(unsigned char r, unsigned char g, unsigned char b) {
 
     return r + 256*g + 65280*b;
 }
 
-int read_INT(const unsigned char *buf, unsigned int *offset) {
+int read_INT(const unsigned char *buf, unsigned long *offset) {
 
     *offset +=4;
-    if (buf[*offset - 1] > 47) {
+    if (buf[*offset - 1] > 15) {
 
-        int x = buf[*offset - 1] << 24 | buf[*offset - 2] << 16 | buf[*offset - 3] << 8 | buf[*offset - 4];
+        long x = buf[*offset - 1] << 24 | buf[*offset - 2] << 16 | buf[*offset - 3] << 8 | buf[*offset - 4];
         x--;
         return -~x;
     } else return buf[*offset - 1] << 24 | buf[*offset - 2] << 16 | buf[*offset - 3] << 8 | buf[*offset - 4];
 }
 
-unsigned short read_USHORT(const unsigned char *buf, unsigned int *offset) {
+unsigned short read_USHORT(const unsigned char *buf, unsigned long *offset) {
 
     *offset += 2;
     return buf[*offset - 1] << 8 | buf[*offset - 2];
 }
 
-unsigned int read_UINT(const unsigned char *buf, unsigned int *offset) {
+unsigned int read_UINT(const unsigned char *buf, unsigned long *offset) {
 
     *offset +=4;
     return buf[*offset - 1] << 24 | buf[*offset - 2] << 16 | buf[*offset - 3] << 8 | buf[*offset - 4];
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
     }
     struct metadata_t meta_first;
     struct metadata_t meta_second;
-    unsigned int offset_first = 0, offset_second = 0;
+    unsigned long offset_first = 0, offset_second = 0;
     fread(buf_first, HEADERS_OFFSET - 16, 1, first);
     fread(buf_second, HEADERS_OFFSET - 16, 1, second);
     if (read_USHORT(buf_first, &offset_first) != 0x4D42 || read_USHORT(buf_second, &offset_second) != 0x4D42) {
@@ -122,7 +122,7 @@ int main(int argc, char **argv) {
         meta_second.image_size = abs(meta_second.bitmap_height) * meta_second.bitmap_width;
     } else {
 
-        unsigned int row_size = (meta_first.bits_per_pixel * meta_first.bitmap_width + 31) / 32 * 4;
+        unsigned long row_size = (meta_first.bits_per_pixel * meta_first.bitmap_width + 31) / 32 * 4;
         meta_first.image_size = abs(meta_first.bitmap_height) * row_size;
         meta_second.image_size = abs(meta_second.bitmap_height) * row_size;
     }
@@ -142,31 +142,26 @@ int main(int argc, char **argv) {
         for (unsigned int i = 4; i < 1025; i += 4) {
 
             first_bmp.palette[i / 4 - 1] = hash(buf[i - 2], buf[i - 3], buf[i - 4]);
-            if (buf[i - 1] != 0) {
-
-                fprintf(stderr, "First file has wrong color palette!");
-                return -1;
-            }
         }
         free(buf);
         buf = malloc(meta_first.image_size);
         fread(buf, meta_first.image_size, 1, first);
-        first_bmp.pixel_array = malloc(meta_first.image_size * 4);
-        for (unsigned int i = 0; i < meta_first.image_size; i++) {
+        first_bmp.pixel_array = malloc(meta_first.image_size * sizeof(long));
+        for (unsigned long i = 0; i < meta_first.image_size; i++) {
 
             first_bmp.pixel_array[i] = first_bmp.palette[buf[i]];
         }
         free(buf);
     } else {
 
-        unsigned int row_size = (meta_first.bits_per_pixel * meta_first.bitmap_width + 31) / 32 * 4;
-        first_bmp.pixel_array = malloc(abs(meta_first.bitmap_height) * meta_first.bitmap_width * sizeof(int));
+        unsigned long row_size = (meta_first.bits_per_pixel * meta_first.bitmap_width + 31) / 32 * 4;
+        first_bmp.pixel_array = malloc(abs(meta_first.bitmap_height) * meta_first.bitmap_width * sizeof(long));
         unsigned char *buf = malloc(meta_first.image_size);
         fread(buf, meta_first.image_size, 1, first);
-        unsigned int counter = 0; unsigned int global_i = 0;
-        for (unsigned j = 0; j < abs(meta_first.bitmap_height); j++) {
+        unsigned long counter = 0, global_i = 0;
+        for (unsigned long j = 0; j < abs(meta_first.bitmap_height); j++) {
 
-            for (unsigned int i = 0; i < meta_first.bitmap_width; i++) {
+            for (unsigned long i = 0; i < meta_first.bitmap_width; i++) {
 
                 first_bmp.pixel_array[counter] = hash(buf[global_i + i*3 + 2], buf[global_i + i*3 + 1], buf[global_i + i*3]);
                 counter++;
@@ -178,35 +173,30 @@ int main(int argc, char **argv) {
     if (meta_second.bits_per_pixel == 8) {
 
         unsigned char *buf = malloc(1024);
-        fread(buf, 1024, 1, first);
+        fread(buf, 1024, 1, second);
         for (unsigned int i = 4; i < 1025; i += 4) {
 
             second_bmp.palette[i / 4 - 1] = hash(buf[i - 2], buf[i - 3], buf[i - 4]);
-            if (buf[i - 1] != 0) {
-
-                fprintf(stderr, "Second file has wrong color palette!");
-                return -1;
-            }
         }
         free(buf);
         buf = malloc(meta_second.image_size);
-        fread(buf, meta_second.image_size, 1, first);
-        second_bmp.pixel_array = malloc(meta_second.image_size);
-        for (unsigned int i = 0; i < meta_second.image_size; i++) {
+        fread(buf, meta_second.image_size, 1, second);
+        second_bmp.pixel_array = malloc(meta_second.image_size * sizeof(long));
+        for (unsigned long i = 0; i < meta_second.image_size; i++) {
 
             second_bmp.pixel_array[i] = second_bmp.palette[buf[i]];
         }
         free(buf);
     } else {
 
-        unsigned int row_size = (meta_second.bits_per_pixel * meta_second.bitmap_width + 31) / 32 * 4;
-        second_bmp.pixel_array = malloc(row_size * abs(meta_second.bitmap_height) * sizeof(int));
+        unsigned long row_size = (meta_second.bits_per_pixel * meta_second.bitmap_width + 31) / 32 * 4;
+        second_bmp.pixel_array = malloc(row_size * abs(meta_second.bitmap_height) * sizeof(long));
         unsigned char *buf = malloc(meta_second.image_size);
         fread(buf, meta_second.image_size, 1, second);
-        unsigned int counter = 0; unsigned int global_i = 0;
-        for (unsigned j = 0; j < abs(meta_second.bitmap_height); j++) {
+        unsigned long counter = 0, global_i = 0;
+        for (unsigned long j = 0; j < abs(meta_second.bitmap_height); j++) {
 
-            for (unsigned int i = 0; i < meta_second.bitmap_width; i++) {
+            for (unsigned long i = 0; i < meta_second.bitmap_width; i++) {
 
                 second_bmp.pixel_array[counter] = hash(buf[global_i + i*3 + 2], buf[global_i + i*3 + 1], buf[global_i + i*3]);
                 counter++;
@@ -216,17 +206,17 @@ int main(int argc, char **argv) {
         free(buf);
     }
     unsigned char error = 0;
-    int all_pixels = abs(meta_first.bitmap_height) * meta_first.bitmap_width;
-    for (unsigned int y = 0; y < abs(meta_first.bitmap_height); y++) {
+    unsigned long all_pixels = abs(meta_first.bitmap_height) * meta_first.bitmap_width;
+    for (unsigned long y = 0; y < abs(meta_first.bitmap_height); y++) {
 
-        for (unsigned int x = 0; x < meta_first.bitmap_height; x++) {
+        for (unsigned long x = 0; x < meta_first.bitmap_width; x++) {
 
-            int a, b;
+            long a, b;
             if (meta_first.bitmap_height < 0) a = all_pixels - (y + 1) * meta_first.bitmap_width + x; else a = y * meta_first.bitmap_width + x;
             if (meta_second.bitmap_height < 0) b = all_pixels - (y + 1) * meta_second.bitmap_width + x; else b = y * meta_second.bitmap_width + x;
             if (first_bmp.pixel_array[a] != second_bmp.pixel_array[b]) {
 
-                printf("%u %u\n", x, y);
+                printf("%lu %lu\n", x, y);
                 error++;
                 if (error == MAX_UNEQUAL_COORDINATES + 1) return -1;
             }
